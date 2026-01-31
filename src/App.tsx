4b1b6936920202c54427c050115371f724809b66
hana15-cmd <hanaqureshi46@gmail.com>
2026-01-31 12:58:57 +0000

    Some changes

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function AccountingApp() {
+  return (
+    <div>
+      <h1>Accounting MFE</h1>
+      <p>This is loaded from a remote repo</p>
+    </div>
+  )
+}
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import federation from "@originjs/vite-plugin-federation";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "mfe_accounting_app",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App",
+      },
+      shared: ["react", "react-dom","react-router-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+  preview: {
+    port: 5002,
+    strictPort: true,
+    cors: true,
+  },
+});
